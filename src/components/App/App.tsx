322e5a79403fa404a6d7dd70b92c22e1b7779380
MrLuigiG <luigi@honeygrid.ai>
2024-02-13 14:27:11 -0600

    added menu, TODO link menu to scroll

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,36 @@
-import React, { FC } from "react";
-import { Provider, defaultTheme, Grid, View, Text, Flex, Image } from "@adobe/react-spectrum";
+import React, { FC, useRef } from "react";
+import {
+    Provider,
+    defaultTheme,
+    Grid,
+    View,
+    Text,
+    Flex,
+    Image,
+    Menu,
+    MenuTrigger,
+    ActionButton,
+    Item,
+} from "@adobe/react-spectrum";
 import img from "./Headshot.jpeg";
+import ShowMenu from "@spectrum-icons/workflow/ShowMenu";
 
 import "./App.css";
 const headShot = require("./Headshot.jpeg");
 
 const App: FC = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    const handleClick = () => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
     return (
         <Provider theme={defaultTheme}>
-            <Grid areas={["header", "content", "footer"]} columns={["1fr"]} rows={["auto", "85vh", "size-1000"]}>
+            <Grid
+                areas={["header", "about-me", "experience", "education", "contact-me"]}
+                columns={["1fr"]}
+                rows={["auto", "85vh", "100vh", "100vh", "25vh"]}
+            >
                 <Grid areas={["spacer heading menu"]} columns={["300px", "auto", "300px"]} rows="auto" gap={"size-0"}>
                     <View gridArea="heading">
                         <Flex justifyContent="center">
@@ -22,8 +44,31 @@ const App: FC = () => {
                             </Text>
                         </Flex>
                     </View>
+                    <View gridArea={"menu"}>
+                        <Flex justifyContent={"right"} alignContent={"safe center"} marginEnd={"2%"} marginTop={"1%"}>
+                            <MenuTrigger>
+                                <ActionButton>
+                                    <ShowMenu aria-label="Show Menu" />
+                                </ActionButton>
+                                <Menu
+                                    onAction={key => {
+                                        if (key === "About me") {
+                                            handleClick();
+                                        } else {
+                                            alert(key);
+                                        }
+                                    }}
+                                >
+                                    <Item key="About me">About me</Item>
+                                    <Item key="Experience">Experience</Item>
+                                    <Item key="Education">Education</Item>
+                                    <Item key="Contact me">Contact me</Item>
+                                </Menu>
+                            </MenuTrigger>
+                        </Flex>
+                    </View>
                 </Grid>
-                <Grid areas={["content-left content-right"]} columns={["1fr 1fr"]} gridArea="content">
+                <Grid areas={["content-left content-right"]} columns={["1fr 1fr"]} gridArea="about-me">
                     <View backgroundColor="chartreuse-600" gridArea="content-left">
                         <Flex
                             justifyContent="center"
@@ -51,8 +96,21 @@ const App: FC = () => {
                             </Text>
                         </Flex>
                     </View>
-                </Grid>{" "}
-                <View backgroundColor="gray-75" gridArea="footer" />
+                </Grid>
+
+                <View backgroundColor="chartreuse-400" gridArea="experience">
+                    {/* Placeholder for experience section */}
+
+                    <Text>Experience section</Text>
+                </View>
+                <View backgroundColor="chartreuse-400" gridArea="education">
+                    {/* Placeholder for education section */}
+                    <Text>Education section</Text>
+                </View>
+                <View backgroundColor="chartreuse-400" gridArea="contact-me">
+                    {/* Placeholder for contact-me section */}
+                    <Text>Contact me section</Text>
+                </View>
             </Grid>
         </Provider>
     );
